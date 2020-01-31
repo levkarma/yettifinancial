d3205fc8efe0c8ba54f282d8c20b3c24042477f1
levkarma <levko.k9@gmail.com>
2020-01-31 15:44:49 -0500

    Forgot to add new files.

---
+++ components/NavBar.vue
@@ -0,0 +1,67 @@
+<template>
+	<nav
+		class="navbar is-fixed-top"
+		role="navigation"
+		aria-label="main navigation"
+	>
+		<div class="navbar-brand">
+			<a class="navbar-item" href="/">
+				<img src="~/assets/img/yetti-logo.png" />
+			</a>
+
+			<a
+				role="button"
+				class="navbar-burger burger"
+				aria-label="menu"
+				aria-expanded="false"
+				data-target="navbarBasicExample"
+				@click="$emit('toggleMenuActive')"
+			>
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+			</a>
+		</div>
+
+		<div
+			id="navbarBasicExample"
+			class="navbar-menu"
+			:class="{ 'is-active': menuActive }"
+		>
+			<div class="navbar-start">
+				<a class="navbar-item">
+					About
+				</a>
+				<a class="navbar-item">
+					Financial Services
+				</a>
+				<a class="navbar-item">
+					Coaching and Mentoring
+				</a>
+				<a class="navbar-item">
+					Upcoming Events
+				</a>
+			</div>
+
+			<div class="navbar-end">
+				<div class="navbar-item">
+					<div class="buttons">
+						<a class="button is-primary">
+							<strong>Free Consultation</strong>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+</template>
+<script>
+export default {
+	name: 'NavBar',
+	props: ['menuActive'],
+	data() {
+		return {}
+	}
+}
+</script>
+<style></style>
---
+++ pages/events.vue
@@ -0,0 +1,28 @@
+<template>
+	<section id="events" class="section">
+		<nuxt-link to="/" class="level is-mobile">
+			<div class="level-left">
+				<div class="level-item">
+					<span class="icon"
+						><i class="fas fa-chevron-circle-left fa-lg"></i
+					></span>
+				</div>
+				<div class="level-item">
+					<h1 class="title is-1">Events</h1>
+				</div>
+			</div>
+		</nuxt-link>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'events',
+
+	data() {
+		return {}
+	}
+}
+</script>
+
+<style lang="css" scoped></style>
