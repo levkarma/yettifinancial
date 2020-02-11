c0c9982d5793619c2905a1b53017a76973ad5552
levkarma <levko.k9@gmail.com>
2020-02-10 19:03:58 -0500

    Separate pages for services

---
+++ pages/services/_name.vue
@@ -0,0 +1,46 @@
+<template>
+	<section class="section" id="service">
+		<div class="level is-mobile">
+			<div class="level-left">
+				<nuxt-link class="level-item button is-primary" to="/#services">
+					<span class="icon fas fa-arrow-left"></span><span>Services</span>
+				</nuxt-link>
+			</div>
+		</div>
+		<div class="columns">
+			<div class="column is-one-quarter">
+				<figure class="image is-1by1">
+					<img :src="service.image" alt="" />
+				</figure>
+			</div>
+			<div class="column is-three-quarter">
+				<div class="content">
+					<h2>{{ service.name }}</h2>
+					<div v-html="service.description"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+const servicesData = require('~/assets/data/services-data.json')
+export default {
+	name: 'ServicePage',
+	props: ['fromRoute'],
+	computed: {
+		service() {
+			return servicesData.filter(service => {
+				console.log(service.name, this.$route.params.name.split('_').join(' '))
+				return service.name === this.$route.params.name.split('_').join(' ')
+			})[0]
+		}
+	},
+	data() {
+		return {}
+	},
+	mounted() {}
+}
+</script>
+
+<style lang="css" scoped></style>
