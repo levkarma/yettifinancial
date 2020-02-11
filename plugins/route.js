95fa2d5ddd464466b55fbaecf12a07d1964f3699
levkarma <levko.k9@gmail.com>
2020-02-11 17:12:15 -0500

    Cache issues, document body too

--- plugins/route.js
+++ plugins/route.js
@@ -2,7 +2,8 @@ export default ({ store, app }) => {
 	// 	// Every time the route changes (fired on initialization too)
 	if (process.client) {
 		app.router.beforeEach((to, from, next) => {
-			console.log('in plugin', document.querySelector('html').scrollTop)
+			console.log('in plugin html', document.querySelector('html').scrollTop)
+			console.log('in plugin body', document.querySelector('body').scrollTop)
 			from = {
 				name: from.name,
 				path: from.path,
