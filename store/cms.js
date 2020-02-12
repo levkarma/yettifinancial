280f77b87ea45163ebd6d6a0c077d68ddf8451a9
levkarma <levko.k9@gmail.com>
2020-02-12 17:54:47 -0500

    Change content path.

--- store/cms.js
+++ store/cms.js
@@ -9,17 +9,17 @@ export const mutations = {
 }
 
 export const actions = {
-  async nuxtServerInit({ commit }) {
-    let files = await require.context(
-      '~/assets/content/event/',
-      false,
-      /\.json$/
-    )
-    let blogPosts = files.keys().map(key => {
-      let res = files(key)
-      res.slug = key.slice(2, -5)
-      return res
-    })
-    await commit('setBlogPosts', blogPosts)
-  }
+  // async nuxtServerInit({ commit }) {
+  //   let files = await require.context(
+  //     '~/assets/content/event/',
+  //     false,
+  //     /\.json$/
+  //   )
+  //   let blogPosts = files.keys().map(key => {
+  //     let res = files(key)
+  //     res.slug = key.slice(2, -5)
+  //     return res
+  //   })
+  //   await commit('setBlogPosts', blogPosts)
+  // }
 }
